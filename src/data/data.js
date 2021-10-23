f3bdaba5b47f0e2e4378aacda27abb4f0f72b8b2
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-10-23 00:33:53 -0700

    fix typo

--- src/data/data.js
+++ src/data/data.js
@@ -19,7 +19,7 @@ export const projects = [
     title: "BiOptimizers",
     subtitle: "BiOptimizers Video Catalogue Page",
     description:
-      "Project for BiOptimizers USA, includes google ads API, youtube API, and tested for optimization via Lighthouse tool. Made with React and hosted via Vercel.",
+      "Project for BiOptimizers USA, includes google ads API, youtube API, and tested for optimization via Lighthouse tool. Made with React.",
     image: "https://i.imgur.com/yV2016R.png",
     link: "https://github.com/TylerJVannoy/React-youtube",
   },
